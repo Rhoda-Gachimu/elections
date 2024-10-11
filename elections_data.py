02283d51283735d3b30f8b639279634321dfa6a4
Rhoda Gachimu <gachimurhoda@gmail.com>
2024-10-11 16:25:57 +0300

    chore:rename columns

--- elections_data.py
+++ elections_data.py
@@ -4,7 +4,17 @@ import os
 # import arcpy
 
 
+rename_cols = {
+    "state_po":"state_abbr",
+    "county_fips":"FIPS",
+    "party":"pol_identity"
+}
+
 elections_data_path = "DATA\countypres_2000-2020.csv"
-election_results = pd.read_csv(elections_data_path)
+election_results = pd.read_csv(elections_data_path, dtype={"county_fips":object})
+election_results.rename(columns=rename_cols, inplace=True)
+election_results.head()
+
 
 print(election_results)
+print()
